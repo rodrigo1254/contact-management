ca6cb649777216f3e2efbac4b152668a5e1dd61c
Rodrigo Campos <rodrigo1254@gmail.com>
2024-02-02 16:49:13 -0300

    rota api para melhor entendimento

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ use App\Http\Controllers\ContactController;
 
 
 Route::prefix('api')->group(function () {
-    Route::get('/', [ContactController::class, 'index'])->name('contacts.index');
+    Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
     Route::resource('contacts', ContactController::class)->except(['index']);
 });
