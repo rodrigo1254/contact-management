118efaf0bf3a18fac70e7d4995c9c2e4a88f2f7d
Rodrigo Campos <rodrigo1254@gmail.com>
2024-02-02 15:08:17 -0300

    use home controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+
 
 /*
 |--------------------------------------------------------------------------
