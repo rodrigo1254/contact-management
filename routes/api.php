156e0750907fec9418d8e8ce90ccfd26ddca116d
Rodrigo Campos <rodrigo1254@gmail.com>
2024-02-02 16:50:51 -0300

    ajuste rotas api

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
 
-Route::prefix('api')->group(function () {
+##Route::prefix('api')->group(function () {
     Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
     Route::resource('contacts', ContactController::class)->except(['index']);
-});
+##});
