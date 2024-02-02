4439af0d96269f41450417fd63b677ac7f4acfae
Rodrigo Campos <rodrigo1254@gmail.com>
2024-02-02 15:10:38 -0300

    ajuste rotas

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ContactController;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-/*Route::get('/', function () {
-    return view('list');
-});*/
-
-/* Rota para visualizar a lista de contatos */
-//Route::get('/', [ContactController::class, 'index'])->name('contacts.index');
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
-
-/* Rotas para as operações CRUD de contatos (Create, Read, Update, Delete) */
 Route::resource('contacts', ContactController::class)->except(['index']);
 
-/* Rotas de Autenticação (Opcionais) */
 if (env('ENABLE_AUTH', false)) {
     Auth::routes();
 }
