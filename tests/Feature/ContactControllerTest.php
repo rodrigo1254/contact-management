01b0284409c7ddec52cfde484df6ce7174f7f495
Rodrigo Campos <rodrigo1254@gmail.com>
2024-02-02 14:36:07 -0300

    use DatabaseTransactions

--- tests/Feature/ContactControllerTest.php
+++ tests/Feature/ContactControllerTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Contact;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ContactControllerTest extends TestCase
 {
