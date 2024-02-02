3130f09b43c96698c37398c8d28101a80f81c9c6
Rodrigo Campos <rodrigo1254@gmail.com>
2024-02-02 11:33:20 -0300

    setando campos da nova tabela

--- database/migrations/2024_02_02_143123_create_contacts_table.php
+++ database/migrations/2024_02_02_143123_create_contacts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('nome', 255);
+            $table->string('contato', 9);
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
