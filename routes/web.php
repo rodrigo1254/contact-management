c9208cf878311e261d2d7e1ffd60c9d93ab87dfb
Rodrigo Campos <rodrigo1254@gmail.com>
2024-02-02 15:14:19 -0300

    ajuste rotas

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ContactController;
 
+Route::group(['middleware' => 'web'], function () {
+    // Suas rotas aqui
+});
+
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
 Route::resource('contacts', ContactController::class)->except(['index']);
