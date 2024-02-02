fe95b4f6cafb866e1a80b78ba76cd58c9a5b9159
Rodrigo Campos <rodrigo1254@gmail.com>
2024-02-02 11:57:18 -0300

    configuracoes para listagem dos contatos + mais view em diretorio padrao de contatos

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,10 +1,55 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Contact;
 
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    public function index()
+    {
+        $contacts = Contact::all();
+        return view('contacts.index', compact('contacts'));
+    }
+
+    public function create()
+    {
+        return view('contacts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nome' => 'required|min:5',
+            'contato' => 'required|digits:9',
+            'email' => 'required|email|unique:contacts',
+        ]);
+
+        Contact::create($request->all());
+        return redirect()->route('contacts.index')->with('success', 'Contato criado com sucesso!');
+    }
+
+    public function edit(Contact $contact)
+    {
+        return view('contacts.edit', compact('contact'));
+    }
+
+    public function update(Request $request, Contact $contact)
+    {
+        $request->validate([
+            'nome' => 'required|min:5',
+            'contato' => 'required|digits:9',
+            'email' => 'required|email|unique:contacts,email,' . $contact->id,
+        ]);
+
+        $contact->update($request->all());
+        return redirect()->route('contacts.index')->with('success', 'Contato atualizado com sucesso!');
+    }
+
+    public function destroy(Contact $contact)
+    {
+        $contact->delete();
+        return redirect()->route('contacts.index')->with('success', 'Contato excluído com sucesso!');
+    }
 }
