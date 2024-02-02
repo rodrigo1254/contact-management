3993864fca8bee175f395f1c11ebefb6b8f118d4
Rodrigo Campos <rodrigo1254@gmail.com>
2024-02-02 15:15:53 -0300

    ajuste rotas

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ContactController;
 
-Route::group(['middleware' => 'web'], function () {
-    // Suas rotas aqui
-});
-
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
 Route::resource('contacts', ContactController::class)->except(['index']);
 
 if (env('ENABLE_AUTH', false)) {
+    // Remova o grupo de middleware 'web'
     Auth::routes();
 }
