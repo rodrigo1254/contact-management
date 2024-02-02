6e730f53b66f4726bd880db6e7801369a47b44f7
Rodrigo Campos <rodrigo1254@gmail.com>
2024-02-02 17:14:13 -0300

    limpando o codigo

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
-
-##Route::prefix('api')->group(function () {
-    Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
-    Route::resource('contacts', ContactController::class)->except(['index']);
-##});
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+Route::resource('contacts', ContactController::class)->except(['index']);
--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,6 @@ use App\Http\Controllers\AuthLoginController;
 Route::get('/', [ContactController::class, 'index'])->name('contacts.index');
 Route::resource('contacts', ContactController::class)->except(['index']);
 
-// Rotas relacionadas ao login/logout
 Route::post('/login', [AuthLoginController::class, 'login'])->name('login');
 Route::post('/logout', [AuthLoginController::class, 'logout'])->name('logout');
 
-
-/*
-Route::resource('/', ContactController::class);
-
-Route::get('/', [ContactController::class, 'index'])->name('contacts.index');
-Route::resource('contacts', ContactController::class)->except(['index']);
-
-Route::post('/login', [AuthLoginController::class, 'login'])->name('login');
-
-Route::post('/logout', [AuthLoginController::class, 'logout'])->name('logout');
-*/
